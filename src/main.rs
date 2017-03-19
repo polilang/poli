0f280b4ab15b48cde1ec1a34c185026f4c3fa268
nilq <i.am@nilq.dk>
2017-03-19 21:09:32 +0000

    Implement test REPL and file loader

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,55 @@
-extern crate clap;
-use clap::App;
+use std::io;
+use std::io::prelude::*;
+
+use std::fs::File;
+
+use std::env;
+
+fn repl_loop() {
+    println!("the poli language\n");
+
+    loop {
+        print!(">>> ");
+
+        io::stdout().flush().unwrap();
+
+        let mut input_line = String::new();
+
+        match io::stdin().read_line(&mut input_line) {
+            Ok(_)  => {
+                if input_line.trim() == String::from(":quit") ||
+                   input_line.trim() == String::from(":q") 
+                {
+                    println!("=> bye bb <3");
+
+                    std::process::exit(0)
+                }
+
+                println!("=> {}", input_line.trim());
+            },
+
+            Err(e) => panic!(e),
+        }
+    }
+}
 
 fn main() {
-    App::new("the poli language")
-        .version("v0.0-dev")
-        .about("a compiled programming language")
-        .args_from_usage(
-            "<input>              'specifies input source file'")
-        .get_matches();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let path = &args[1];
+
+        let mut source_file = match File::open(path) {
+            Ok(f)  => f,
+            Err(_) => panic!("failed to open path: {}", path),
+        };
+
+        let mut source_buffer = String::new();
+        source_file.read_to_string(&mut source_buffer).unwrap();
+
+        println!("=> {}", source_buffer)
+        
+    } else {
+        repl_loop();
+    }
 }
