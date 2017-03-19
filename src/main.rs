4d8576be705a8ed16975638836199ee40596c0a5
nilq <i.am@nilq.dk>
2017-03-19 19:00:50 +0000

    Add simple argument structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("the poli language")
+        .version("v0.0-dev")
+        .about("a compiled programming language")
+        .args_from_usage(
+            "<input>              'specifies input source file'")
+        .get_matches();
 }
