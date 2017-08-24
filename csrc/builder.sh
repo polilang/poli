8e71431e39fc4b39b7eb7aeacd801e6c0ee34f7f
evolbug <daniels.kursits@gmail.com>
2017-08-24 17:21:56 +0300

    added stuff to builder

--- csrc/builder.sh
+++ csrc/builder.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-inotifywait -q -m -e close_write lexer.c parser.c|
+inotifywait -q -m -e close_write parser.c lexer.c ../examples ./templates|
 while read -r filename event; do
    kill `ps aux | grep [v]algrind | awk '{print $2}' | tail -n1`
    clear
+   pause .5s
    cc -Wall -g parser.c && valgrind -q --show-leak-kinds=all ./a.out &
 done
